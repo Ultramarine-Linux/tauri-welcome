d8da31882a2c6c8be69404b375b6e5aa240a09be
trobonox <57040351+trobonox@users.noreply.github.com>
2022-06-08 19:50:16 +0200

    Add simple fade page transition

--- app.vue
+++ app.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>
-    <NuxtLayout>
-      <NuxtPage/>
-    </NuxtLayout>
-  </div>
+    <div>
+        <NuxtLayout>
+            <NuxtPage/>
+        </NuxtLayout>
+    </div>
 </template>
+
+<script setup>
+onMounted (() => {
+    document.body.classList.add('bg-libadw-bg')
+})
+</script>
+
+
+<style>
+    .page-enter-active,
+    .page-leave-active {
+        transition: opacity 0.3s ease;
+    }
+
+    .page-enter-from,
+    .page-leave-to {
+        opacity: 0;
+    }
+</style>
