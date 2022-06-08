01470e2950e2c87dd6bb2babe0f9fe434b972854
trobonox <57040351+trobonox@users.noreply.github.com>
2022-06-08 18:47:43 +0200

    Adjust color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 module.exports = {
-  content: [],
-  theme: {
-      extend: {
-          colors: {
-              gray: {
-                  900: "#242424",
-                  800: "#ffffff04",
-                  700: "#5e5c64",
-                  600: "#77767b"
+    content: [],
+    theme: {
+        extend: {
+            colors: {
+                "libadw": {
+                    "bg": "#242424",
+                    "btn": "#3a3a3a",
+                    "btn-hover": "#626262",
+                    "bg-darker": "#303030",
+                    "input": "#1e1e1e"
+                }
             }
-        }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
