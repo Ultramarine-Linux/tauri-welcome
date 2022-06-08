53ebb870ea47deb4857e8b450234c8ee9f3af3b5
trobonox <57040351+trobonox@users.noreply.github.com>
2022-06-08 18:47:02 +0200

    Remove unused css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,5 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss'],
-    css: ['@/assets/main.css']
+    modules: ['@nuxtjs/tailwindcss']
 })
