9787250b26a6a4d81acf101163703e2d0547cc67
Trobo <57040351+trobonox@users.noreply.github.com>
2022-06-09 15:02:42 +0200

    Add first contents of package install page

--- pages/pkg-install.vue
+++ pages/pkg-install.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="w-full min-h-screen bg-libadw-bg text-white py-8 px-4">
-        <h1 class="font-bold text-4xl">Software installation</h1>
+        <h1 class="font-bold text-4xl mt-[45px]">Software installation</h1>
+        <h2>Install all of your favourite software with the click of a button!</h2>
     </div>
 </template>
