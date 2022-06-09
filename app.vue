3050eedab461637cce176bd5b848c16f01efa03e
Trobo <57040351+trobonox@users.noreply.github.com>
2022-06-09 14:44:03 +0200

    Enable titlebar text to also be draggable

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
                 </svg>
             </a>
-            <h1 class='my-1.5 font-semibold'>Ultramarine Welcome</h1>
+            <h1 data-tauri-drag-region class='my-1.5 font-semibold'>Ultramarine Welcome</h1>
             <button @click="closeApp">
                 <svg class="text-white titlebar-button titlebar-button-close" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);"><path fill="currentColor" d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"/></svg>
             </button>
