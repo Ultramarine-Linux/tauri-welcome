a4e550fecf2286972e3e120ce054cb96e1455a94
Trobo <57040351+trobonox@users.noreply.github.com>
2022-06-09 14:45:34 +0200

    Pointer cursor for back button

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
     <div>  
         <div data-tauri-drag-region class="titlebar">
-            <a @click="$router.go(-1)" class="titlebar-button titlebar-button-back flex justify-center items-center">
+            <a @click="$router.go(-1)" class="titlebar-button titlebar-button-back flex justify-center items-center cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="3">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
                 </svg>
