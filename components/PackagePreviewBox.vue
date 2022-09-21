5ddb7ad2747330ac97116741ddaf3b0dbb9e50c4
Trobo <57040351+trobonox@users.noreply.github.com>
2022-09-21 19:42:29 +0200

    Add base installer page

---
+++ components/PackagePreviewBox.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="p-4 bg-libadw-btn rounded-md">
+        <h1 class="text-lg font-bold tracking-tighter">{{ packageName }}</h1>
+        <p>{{ description }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        packageName: {
+            type: Text,
+            required: true
+        },
+        description: {
+            type: Text,
+            required: true
+        }
+    }
+}
+</script>
--- pages/pkg-install.vue
+++ pages/pkg-install.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="w-full min-h-screen bg-libadw-bg text-white py-8 px-4">
         <h1 class="font-bold text-4xl mt-[45px]">Software installation</h1>
-        <h2>Install all of your favourite software with the click of a button!</h2>
+        <h2 class="mb-8">Install all of your favorite software with the click of a button!</h2>
+        <section id="package-selection" class="grid grid-cols-2 gap-x-12 gap-y-4">
+            <PackagePreviewBox packageName="VSCode" description="Code editor" />
+            <PackagePreviewBox packageName="VSCode" description="Code editor" />
+            <PackagePreviewBox packageName="VSCode" description="Code editor" />
+            <PackagePreviewBox packageName="VSCode" description="Code editor" />
+        </section>
+
     </div>
 </template>
