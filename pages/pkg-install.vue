5bf3e4d6630a39dfd41bd8c21640ae9335d9a308
trobonox <57040351+trobonox@users.noreply.github.com>
2022-09-25 10:31:38 +0200

    Adjust spacing in installer

--- pages/pkg-install.vue
+++ pages/pkg-install.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-full min-h-screen bg-gray-900 text-white py-8 px-4">
-        <h1 class="font-bold text-4xl mt-[45px]">Software installation</h1>
+        <h1 class="font-bold text-4xl">Software installation</h1>
         <h2 class="mb-8">Install all of your favorite software with the click of a button!</h2>
         <section id="package-selection" class="grid grid-cols-2 gap-x-12 gap-y-4">
             <PackagePreviewBox packageName="VSCode" description="Code editor" />
