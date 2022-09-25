fee9a5314750b40542cb9e270b10b644d454e980
trobonox <57040351+trobonox@users.noreply.github.com>
2022-09-25 09:13:22 +0200

    Remove dead code

--- app.vue
+++ app.vue
@@ -5,18 +5,3 @@
         </NuxtLayout>
     </div>
 </template>
-
-<script setup>
-import { appWindow } from '@tauri-apps/api/window'
-
-const closeApp = (() => {
-    appWindow.close()
-})
-</script>
-
-
-<style>
-    html, body {
-        overflow: hidden;
-    }
-</style>
