805fd19789c0c0c6c807b4d69e030ee7e55d7139
trobonox <57040351+trobonox@users.noreply.github.com>
2022-09-25 11:06:26 +0200

    Add yml parser vite plugin and modified package list yml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
+import ViteYaml from '@modyfi/vite-plugin-yaml';
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     modules: ['@nuxtjs/tailwindcss'],
-    ssr: false
+    ssr: false,
+    vite: {
+        plugins: [
+            ViteYaml()
+        ]
+    }
 })
