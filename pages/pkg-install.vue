ec347dad4f24ccdd7d235c2e12d6c7a0e8f6584d
trobonox <57040351+trobonox@users.noreply.github.com>
2022-09-25 11:06:54 +0200

    Parse package yml and display it in tiles

--- pages/pkg-install.vue
+++ pages/pkg-install.vue
@@ -1,12 +1,16 @@
 <template>
-    <div class="w-full min-h-screen bg-gray-900 text-white py-8 px-4">
+    <div class="w-full min-h-screen bg-gray-900 text-white py-8 px-4 h-full">
         <h1 class="font-bold text-4xl">Software installation</h1>
         <h2 class="mb-8">Install all of your favorite software with the click of a button!</h2>
         <section id="package-selection" class="grid grid-cols-2 gap-x-12 gap-y-4">
-            <PackagePreviewBox packageName="VSCode" description="Code editor" />
-            <PackagePreviewBox packageName="VSCode" description="Code editor" />
-            <PackagePreviewBox packageName="VSCode" description="Code editor" />
-            <PackagePreviewBox packageName="VSCode" description="Code editor" />
+            <PackagePreviewBox v-for="pkg in software.packages" :packageName="pkg.name" :description="pkg.description"/>
         </section>
     </div>
 </template>
+
+<script setup>
+import data from "@/software_list.yml"
+import { reactive } from "vue"
+
+const software = reactive(data)
+</script>
