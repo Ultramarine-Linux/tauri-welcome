34c6b4b265bc83b840c96f5d9c89de9ce01ffdba
trobonox <57040351+trobonox@users.noreply.github.com>
2023-06-16 23:36:31 +0200

    Set up new base with Nuxt 3 stable and PrimeVue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
-import { defineNuxtConfig } from 'nuxt'
-
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss'],
-    ssr: false
-})
+  devtools: { enabled: true },
+  ssr: false,
+  css: [
+    "primevue/resources/themes/lara-light-blue/theme.css",
+    "primevue/resources/primevue.css",
+  ],
+  build: {
+    transpile: ["primevue"],
+  },
+});
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,5 @@
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
-
-mod software;
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
   tauri::Builder::default()
